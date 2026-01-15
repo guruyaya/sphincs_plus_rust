56c033ca7f177be8335e7c11939deaaee757675e
yair Eshel <yair@polywizz.co.il>
2026-01-15 03:58:47 +0200

    Done with the initial random gen

--- src/lib/helpers/random_generator.rs
+++ src/lib/helpers/random_generator.rs
@@ -1,11 +1,24 @@
 pub mod random_generator {
+    use sha2::{Sha256, Digest, digest::Update};
+
+    #[derive(Clone, Debug)]
     pub struct Address {
         pub level: u16,
-        pub start_key: u64
+        pub position: u64
     }
+
+    fn get_key(seed: [u8; 32], address: Address) -> [u8;32] {
+        let mut hasher = Sha256::default();
+        Update::update(&mut hasher, &seed);
+        Update::update(&mut hasher, &address.level.to_be_bytes());
+        Update::update(&mut hasher, &address.position.to_be_bytes());
+        let result = hasher.finalize();
+        result.into()
+    }
+
     pub trait RandomGeneratorTrait {
         fn new(seed: [u8; 32]) -> Self;
-        fn get_keys(&mut self, num_keys: u8, address: Address) -> Vec<[u8; 32]>;
+        fn get_keys(&mut self, num_keys: u64, address: Address) -> Vec<[u8; 32]>;
 
     }
 
@@ -17,6 +30,7 @@ pub mod random_generator {
     // impl RandomGeneratorTrait for RandomGenerator32 {
     // }
     
+    #[derive(Clone, Debug)]
     pub struct RandomGenerator64 {
         seed: [u8; 32],
     }
@@ -26,8 +40,14 @@ pub mod random_generator {
             RandomGenerator64 { seed }
         }
 
-        fn get_keys(&mut self, num_keys: u8, address: Address) -> Vec<[u8; 32]> {
-            todo!()
+        fn get_keys(&mut self, num_keys: u64, address: Address) -> Vec<[u8; 32]> {
+            let mut out: Vec<[u8; 32]> = Vec::new();
+            for i in 0..num_keys{
+                let mut new_address = address.clone();
+                new_address.position = new_address.position + i;
+                out.push( get_key(self.seed, new_address) );
+            };
+            out
         }
     }
 }
@@ -37,16 +57,96 @@ mod tests {
     use super::random_generator::{RandomGenerator64, Address, RandomGeneratorTrait};
 
     #[test]
-    fn test_number_of_diffrent_keys() {
-        let address1 = Address {level: 0, start_key: 19};
+    fn test_effect_of_position() {
         let seed:[u8;32] = [0;32];
-
         let mut generator = RandomGenerator64::new(seed);
+        
+        let address1 = Address {level: 0, position: 19};
+        let key_list1 = generator.get_keys(2, address1);
+        
+        assert_eq!(key_list1.len(), 2);
+        assert_ne!(key_list1[0], key_list1[1]);
+        
+        let address2 = Address {level: 0, position: 20};
+        let key_list2 = generator.get_keys(2, address2);
+        
+        assert_eq!(key_list2.len(), 2);
+        assert_ne!(key_list2[0], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[0]);
+        assert_ne!(key_list1[1], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[1]);
+        assert_eq!(key_list1[1], key_list2[0]);
+    }
 
-        let key_list = generator.get_keys(2, address1);
+    #[test]
+    fn test_effect_of_level() {
+        let seed:[u8;32] = [0;32];
+        let mut generator = RandomGenerator64::new(seed);
+        
+        let address1 = Address {level: 0, position: 19};
+        let key_list1 = generator.get_keys(2, address1);
+        
+        assert_eq!(key_list1.len(), 2);
+        assert_ne!(key_list1[0], key_list1[1]);
+        
+        let address2 = Address {level: 1, position: 19};
+        let key_list2 = generator.get_keys(2, address2);
+        
+        assert_eq!(key_list2.len(), 2);
+        assert_ne!(key_list2[0], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[0]);
+        assert_ne!(key_list1[1], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[1]);
+        assert_ne!(key_list1[1], key_list2[0]);
 
-        assert_eq!(key_list.len(), 2);
-        assert_ne!(key_list[0], key_list[1]);
+    }
+
+    #[test]
+    fn test_effect_of_seed() {
+        let seed1:[u8;32] = [0;32];
+        let mut generator1 = RandomGenerator64::new(seed1);
+        
+        let address = Address {level: 0, position: 19};
+        let key_list1 = generator1.get_keys(2, address.to_owned());
+        
+        assert_eq!(key_list1.len(), 2);
+        assert_ne!(key_list1[0], key_list1[1]);
+        
+        let mut seed2:[u8;32] = [0;32];
+        seed2[0] = 1;
+
+        let mut generator2 = RandomGenerator64::new(seed2);
+        let key_list2 = generator2.get_keys(2, address);
+        
+        assert_eq!(key_list2.len(), 2);
+        assert_ne!(key_list2[0], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[0]);
+        assert_ne!(key_list1[1], key_list2[1]);
+        assert_ne!(key_list1[0], key_list2[1]);
+        assert_ne!(key_list1[1], key_list2[0]);
+
+    }
+
+    #[test]
+    fn test_all_the_same() {
+        let seed1:[u8;32] = [0;32];
+        let mut generator1 = RandomGenerator64::new(seed1);
+        
+        let address = Address {level: 0, position: 19};
+        let key_list1 = generator1.get_keys(2, address.to_owned());
+        
+        assert_eq!(key_list1.len(), 2);
+        assert_ne!(key_list1[0], key_list1[1]);
+        
+        let seed2:[u8;32] = [0;32];
+
+        let mut generator2 = RandomGenerator64::new(seed2);
+        let key_list2 = generator2.get_keys(2, address);
+        
+        assert_eq!(key_list2.len(), 2);
+        assert_ne!(key_list2[0], key_list2[1]);
+        assert_eq!(key_list1[0], key_list2[0]);
+        assert_eq!(key_list1[1], key_list2[1]);
     }
 
 
