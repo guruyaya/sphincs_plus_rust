2160c274944e2878d53e10a0ec7815e1dd0c32fd
yair Eshel <yair@polywizz.co.il>
2026-01-24 00:03:07 +0200

    Validate works

--- src/lib/components/merkle_tree/secret.rs
+++ src/lib/components/merkle_tree/secret.rs
@@ -2,34 +2,52 @@ use crate::lib::{
     components::wots_plus::{secret::WotsPlus, signature::WotsPlusSignature}, helpers::{hasher::{HashContext, hash_array}, random_generator::{Address, HASH_DATA_0, HashData}}
 };
 
-pub struct MerkleProof<const STEM_HEIGHT:usize> { // STEM_HEIGHT does not include the root level
+pub struct MerkleProof<const HEIGHT:usize> { // STEM_HEIGHT does not include the root level
     pub public_key: HashData,
     pub signature: WotsPlusSignature,
-    pub merkle_leaves: [HashData;STEM_HEIGHT]
+    pub merkle_leaves: [HashData;HEIGHT]
 }
 
-impl<const STEM_HEIGHT:usize> MerkleProof<STEM_HEIGHT> {
+pub(super) fn pair_keys(keys: &Vec<HashData>) -> Vec<HashData> {
+    assert!(keys.len() % 2 == 0, "Number of keys provided to pair_keys must be devisible by 2");
+    (0..keys.len()).into_iter().step_by(2).map(|idx| {
+        hash_array(&[keys[idx], keys[idx+1]])
+    }).collect()
+}
+
+impl<const HEIGHT:usize> MerkleProof<HEIGHT> {
     fn validate(&self, message: &[u8])-> bool {
-        self.signature.clone().get_expected_public_from_message(message);
-        todo!()
+        let this_signature = self.signature.clone();
+        let mut key = this_signature.get_expected_public_from_message(message);
+        let num_keys = (2 as usize).pow(HEIGHT as u32);
+        let key_idx = this_signature.context.address.position as usize / num_keys;
+        
+        for other_key in self.merkle_leaves {
+            if key_idx % 2 == 1{
+                key = hash_array(&[other_key, key])
+            }else{
+                key = hash_array(&[key, other_key])
+            }
+        };
+        return self.public_key == key
     }
 }
 #[derive(Debug)]
-pub struct MerkleSigner<const TREE_HEIGHT:usize> {
+pub struct MerkleSigner<const HEIGHT:usize> {
     seed: HashData,
     num_trees: u64,
     pub context: HashContext,
     
 }
 
-impl<const STEM_HEIGHT:usize> MerkleSigner<STEM_HEIGHT> {
+impl<const HEIGHT:usize> MerkleSigner<HEIGHT> {
     pub fn new(seed: HashData, context:HashContext) -> Self {
-        let num_trees: u64 = (2 as u64).pow((STEM_HEIGHT) as u32);
+        let num_trees: u64 = (2 as u64).pow((HEIGHT) as u32);
         Self{seed, context, num_trees}
     }
     pub fn get_height(&self) -> usize{
         // returns the full size of the tree, not the stem
-        STEM_HEIGHT
+        HEIGHT
     }
     pub(super) fn generate_lowest_layer(&self) -> Vec<WotsPlus> {
         
@@ -46,17 +64,12 @@ impl<const STEM_HEIGHT:usize> MerkleSigner<STEM_HEIGHT> {
         let wots_idx:usize = self.context.address.position as usize % self.num_trees as usize;
         lowest_layer[wots_idx].clone()
     }
-    pub fn pair_keys(keys: &Vec<HashData>) -> Vec<HashData> {
-        assert!(keys.len() % 2 == 0, "Number of keys provided to pair_keys must be devisible by 2");
-        (0..keys.len()).into_iter().step_by(2).map(|idx| {
-            hash_array(&[keys[idx], keys[idx+1]])
-        }).collect()
-    }
-    pub(super) fn get_proof_and_public_key(&self, lowest_layer: Vec<WotsPlus>) -> (HashData, [HashData;STEM_HEIGHT]){
+
+    pub(super) fn get_proof_and_public_key(&self, lowest_layer: Vec<WotsPlus>) -> (HashData, [HashData;HEIGHT]){
         let mut public_keys: Vec<HashData> = lowest_layer.iter().map(|wots| wots.generate_public_key().public_key).collect();
-        let mut merkle_proof = [HASH_DATA_0;STEM_HEIGHT];
+        let mut merkle_proof = [HASH_DATA_0;HEIGHT];
         let mut hashed_idx = self.context.address.position as usize % self.num_trees as usize;        
-        for i in 0..STEM_HEIGHT {
+        for i in 0..HEIGHT {
             let other_key = if hashed_idx % 2 == 1 {
                 public_keys[hashed_idx - 1]
             } else {
@@ -64,12 +77,12 @@ impl<const STEM_HEIGHT:usize> MerkleSigner<STEM_HEIGHT> {
             };
             merkle_proof[i] = other_key;
             hashed_idx = hashed_idx / 2;
-            public_keys = Self::pair_keys(&public_keys)
+            public_keys = pair_keys(&public_keys)
         };
         assert_eq!(public_keys.len(), 1, "Something horrible just happened");
         (public_keys[0], merkle_proof)
     }
-    pub fn sign(&self, message: &[u8]) -> MerkleProof<STEM_HEIGHT> {
+    pub fn sign(&self, message: &[u8]) -> MerkleProof<HEIGHT> {
         let lowest_layer = self.generate_lowest_layer();
         let signing_wots = self.get_signing_wots(&lowest_layer);
         let (public_key, merkle_leaves) = self.get_proof_and_public_key(lowest_layer);
@@ -89,6 +102,7 @@ macro_rules! merkle_signer {
 
 #[cfg(test)]
 mod tests {
+    use crate::lib::components::merkle_tree::secret::pair_keys;
     use crate::lib::helpers::{hasher::HashContext, random_generator::HASH_DATA_0};
     use crate::lib::{components::merkle_tree::secret::MerkleSigner, helpers::hasher::hash_message};
 
@@ -134,19 +148,19 @@ mod tests {
             hash_message("a".as_bytes()), hash_message("b".as_bytes()), // Note: the first 2 are the same
             hash_message("a".as_bytes()), hash_message("c".as_bytes()),
             hash_message("a".as_bytes()), hash_message("d".as_bytes()));
-        let keys = MerkleSigner::<2>::pair_keys(&to_join);
+        let keys = pair_keys(&to_join);
         assert_eq!(keys.len(), 4);
         assert_eq!(keys[0], keys[1]);
         assert_ne!(keys[0], keys[2]);
         assert_ne!(keys[0], keys[3]);
         assert_ne!(keys[2], keys[3]);
         
-        let more_keys = MerkleSigner::<2>::pair_keys(&keys);
+        let more_keys = pair_keys(&keys);
         
         assert_eq!(more_keys.len(), 2);
         assert_ne!(more_keys[0], more_keys[1]);
         
-        let one_key = MerkleSigner::<2>::pair_keys(&more_keys);
+        let one_key = pair_keys(&more_keys);
         
         assert_eq!(one_key.len(), 1);
 
@@ -160,7 +174,7 @@ mod tests {
                 hash_message("a".as_bytes()), hash_message("b".as_bytes()), // Note: the first 2 are the same
                 hash_message("a".as_bytes()), hash_message("c".as_bytes()),
                 hash_message("a".as_bytes()) );
-            let _ = MerkleSigner::<2>::pair_keys(&to_join);
+            let _ = pair_keys(&to_join);
     }
     #[test]
     fn test_get_signing_wots() {
@@ -178,14 +192,21 @@ mod tests {
     fn test_get_proof_and_public_key() {
         let context = HashContext::default();
         
-        let signer = merkle_signer!(4, HASH_DATA_0, context.clone());
+        let signer = merkle_signer!(4, HASH_DATA_0, context);
         let lowest_layer = signer.generate_lowest_layer();
         let (public_key, merkle_leaves) = signer.get_proof_and_public_key(lowest_layer);
         assert_eq!(merkle_leaves.len(), 4);
         // This checks if the key is stable over tests
         assert_eq!(public_key, [25, 28, 205, 234, 182, 211, 43, 130, 247, 217, 21, 214, 28, 111, 105, 103, 129, 106, 78, 15, 51, 68, 56, 212, 0, 174, 224, 102, 116, 93, 20, 100] );
         
-
+        let mut other_context = HashContext::default();
+        other_context.address.position = 11;
+        
+        let signer = merkle_signer!(4, HASH_DATA_0, other_context);
+        let lowest_layer = signer.generate_lowest_layer();
+        let (other_public_key, merkle_leaves) = signer.get_proof_and_public_key(lowest_layer);
+        assert_eq!(merkle_leaves.len(), 4);
+        assert_eq!(other_public_key, public_key)
     }
     #[test]
     fn test_signature_on_message() {
